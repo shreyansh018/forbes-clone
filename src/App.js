77fcc9d42b02ac468e0852d14b7f8adcd79de910
Shreyansh <shreyansh.singhrajput2019@gmail.com>
2022-12-26 21:50:44 +0530

    final build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Firstbottom from "./Components/firstbottom";
 import Footer from "./Components/footer";
 import Secondbottom from "./Components/secondbottom";
 import Widget from "./Components/widget";
-import Testing from "./Components/testing";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
       <Firstbottom />
       <Widget />
       <Secondbottom />
-      <Testing />
       <Footer />
     </div>
   );
--- src/Components/widget.js
+++ src/Components/widget.js
@@ -6,7 +6,7 @@ function Widget() {
   const [Accordian3, setAccordian3] = useState(false);
   return (
     <div className="p-5">
-      <div className="ml-10 mr-20">
+      <div className="lg:ml-10 lg:mr-20">
         <h1 className="font-bold text-3xl pb-4">Top Cash-Back Credit Cards</h1>
         <div className="rounded-lg border border-blue-300 py-2">
           {/* inside upper div separation */}
@@ -14,23 +14,21 @@ function Widget() {
             {/* left div */}
             <div>
               <div className="grid grid-cols-3 pb-2 mr-20 ml-4">
-                <h1 className="text-xs pt-1 pl-7 pb-1 rounded-lg bg-blue-200 font-semibold text-blue-800">
+                <h1 className="text-xs lg:pt-1 lg:pl-7 lg:pb-1 m-auto rounded-lg bg-blue-200 font-semibold text-blue-800">
                   BEST CASH BACK, BEST NO ANNUAL FEE CARD
                 </h1>
               </div>
 
               <div>
-                <h1 className="pl-7 text-3xl font-bold">Chase Freedom Flex℠</h1>
+                <h1 className="pl-7 text-xl lg:text-3xl font-bold">
+                  Chase Freedom Flex℠
+                </h1>
               </div>
             </div>
 
-            {/* middle div */}
-
-            {/* middle div END */}
-
             {/* right div */}
             <div className="text-center pt-3 ml-20 mr-20">
-              <h1 className="text-white text-2xl font-bold bg-green-500 hover:bg-green-600 hover:duration-200 hover:ease-out duration-200 rounded-lg ml-20 mr-20 p-3 cursor-pointer">
+              <h1 className="text-white text-xl lg:text-2xl font-bold bg-green-500 hover:bg-green-600 hover:duration-200 hover:ease-out duration-200 rounded-lg ml-20 mr-20 lg:p-3 cursor-pointer">
                 Apply Now
               </h1>
 
